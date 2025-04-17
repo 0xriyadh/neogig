19076e04d3d912624d4244f3a8f8aac45c4cff78
ahnafiqbal-git <ahnafiqbal15@gmail.com>
2025-04-17 23:13:17 +0600

    main route updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import { router } from "../config/trpc";
 import { authRouter } from "./auth.routes";
 import { userRouter } from "./user.routes";
+import { companyRouter } from "./company.routes";
+import { jobSeekerRouter } from "./jobSeeker.routes";
 
 export const appRouter = router({
     auth: authRouter,
     user: userRouter,
+    company: companyRouter,
+    jobSeeker: jobSeekerRouter,
 });
 
 export type AppRouter = typeof appRouter;
